ef6e17c7a305889399b94b6371722cf18f097a3e
Vladimir <kundiusvova0@gmail.com>
2026-04-09 02:11:24 +0300

    final version

--- p2/plaintext_attack_Hill_cypher.py
+++ p2/plaintext_attack_Hill_cypher.py
@@ -1,5 +1,5 @@
 
-from hill_cipher import get_key_matrix, get_key_matrix_inv, matrix_to_key, decrypt, get_padded_text
+from hill_cipher import get_key_matrix, get_key_matrix_inv, matrix_to_key, decrypt, get_padded_text, char_to_int, int_to_char
 import numpy as np
 from math import sqrt
 import string
@@ -18,17 +18,30 @@ class test_item():
         self.plaintext = plaintext
         self.cyphertext = cyphertext
 
-def final_output(item_object : test_item, possible_plaintext : str):
-    c_w = 18 #minimal_column_width
-    if c_w < len(item.key):
-        c_w += (len(item.key) - c_w) + 2
+def final_output(item : test_item, possible_plaintext : str, mode="1"):
 
-    print(f"{"Key":<{c_w}} | {"Real text":<{c_w}} | {"Ciphertext":<{c_w}} | {"Pos. plaintext":<{c_w}}")
-    print(f"{item.key:<{c_w}} | {item.plaintext[:len(item.key)] + "...":<{c_w}} | {item.cyphertext[:len(item.key)] + "...":<{c_w}} | {possible_plaintext[:len(item.key)] + "...":<{c_w}}")
-    print(f"Does the texts match? -> {get_padded_text(item.plaintext, item.block) == possible_plaintext}")
+    c_w = 18 #minimal_column_width
+
+    if mode == "1":
+
+        if c_w < len(item.key):
+            c_w += (len(item.key) - c_w) + 2
+
+        print(f"{"Key":<{c_w}} | {"Real text":<{c_w}} | {"Ciphertext":<{c_w}} | {"Pos. plaintext":<{c_w}}")
+        print(f"{item.key:<{c_w}} | {item.plaintext[:len(item.key)] + "...":<{c_w}} | {item.cyphertext[:len(item.key)] + "...":<{c_w}} | {possible_plaintext[:len(item.key)] + "...":<{c_w}}")
+        print(f"Does the texts match? -> {get_padded_text(item.plaintext, item.block) == possible_plaintext}")
+
+    elif mode == "2":
+
+        if c_w < ((len(item.key)*2) + 3):
+            c_w += (((len(item.key)*2) + 3) - c_w) + 2
+        
+        print(f"{"Key 1":<{c_w}} | {"Key 2":<{c_w}} | {"Real text":<{c_w}} | {"Ciphertext":<{c_w}} | {"Pos. plaintext":<{c_w}} ")
+        print(f"{item.key:<{c_w}} | {item.key2:<{c_w}} | {item.plaintext[:len(item.key)*2] + "...":<{c_w}} | {item.cyphertext[:len(item.key)*2] + "...":<{c_w}} | {possible_plaintext[:len(item.key)*2] + "...":<{c_w}}")
+        print(f"Does the texts match? -> {get_padded_text(item.plaintext, item.block) == possible_plaintext}")
+
 
 test_objects = []
-
 
 
 
@@ -42,6 +55,8 @@ with open('test_text.txt', 'r', encoding='utf-8') as file:
             test_objects.append(test_item(temp[0], temp[1], temp[3], temp[4], key2=temp[2]))
             # type_of_cipher | key_1 | key_2 | plaintext | cyphertext
 
+mode1_items = [obj for obj in test_objects if obj.type_of_cipher == "1"]
+mode2_items = [obj for obj in test_objects if obj.type_of_cipher == "2"]
 
 
 """
@@ -50,7 +65,8 @@ K * P = C
 K = C * P^(-1)
 
 """
-for item in test_objects:
+print("Просто шифр Хилла")
+for item in mode1_items:
     item: test_item
 
     if item.type_of_cipher == "1":
@@ -64,23 +80,61 @@ for item in test_objects:
         possible_key_matrix = np.linalg.matmul(ciphertext_matrix, known_plaintext_matrix_inv) % M
         possible_plaintext = decrypt(item.cyphertext, matrix_to_key(possible_key_matrix), "1") 
 
-        final_output(item, possible_plaintext)
         print("-" * 40)
+        final_output(item, possible_plaintext, mode="1")
+        print("-" * 40)
+        print()
 
 
-
+print("Рекуррентный шифр Хилла")
+if len(mode2_items) >= 3:
+    block = mode2_items[0].block
     
-    if item.type_of_cipher == "2":
-        ciphertext_matrix = np.transpose(get_key_matrix(item.cyphertext[:len(item.key)], item.block))
+    #K1
+    P1_cols = []
+    C1_cols = []
+    
+    for i in range(3):
+        p_block = [char_to_int(c) for c in mode2_items[i].plaintext[:block]]
+        c_block = [char_to_int(c) for c in mode2_items[i].cyphertext[:block]]
+        P1_cols.append(p_block)
+        C1_cols.append(c_block)
+    
+    P1_mat = np.array(P1_cols).T
+    C1_mat = np.array(C1_cols).T
+    
+    try:
+        P1_inv = get_key_matrix_inv(matrix_to_key(P1_mat), block)
+        possible_K1 = np.linalg.matmul(C1_mat, P1_inv) % M
+    except ValueError:
+        print("Матрица P1 неинвертируема. Нужно выбрать другие сообщения.")
+
+    #K2
+    P2_cols = []
+    C2_cols = []
+    
+    for i in range(3):
+        p_block = [char_to_int(c) for c in mode2_items[i].plaintext[block : block*2]]
+        c_block = [char_to_int(c) for c in mode2_items[i].cyphertext[block : block*2]]
+        P2_cols.append(p_block)
+        C2_cols.append(c_block)
         
-        known_plaintext = item.plaintext[:len(item.key)]
-        known_plaintext_matrix = np.transpose(get_key_matrix(known_plaintext, item.block))
-        known_plaintext_matrix_inv = get_key_matrix_inv(matrix_to_key(known_plaintext_matrix), item.block)
-
-
-        possible_key_matrix = np.linalg.matmul(ciphertext_matrix, known_plaintext_matrix_inv) % M
-
-        # ---
-        ciphertext_matrix_2 = np.transpose(get_key_matrix(item.cyphertext[:len(item.key2)], item.block))
-
+    P2_mat = np.array(P2_cols).T
+    C2_mat = np.array(C2_cols).T
     
+    try:
+        P2_inv = get_key_matrix_inv(matrix_to_key(P2_mat), block)
+        possible_K2 = np.linalg.matmul(C2_mat, P2_inv) % M
+    except ValueError:
+        print("Матрица P2 неинвертируема.")
+
+
+    key1_str = matrix_to_key(possible_K1)
+    key2_str = matrix_to_key(possible_K2)
+
+    for item in mode2_items:
+        possible_plaintext = decrypt(item.cyphertext, key1_str, "2", key2=key2_str)
+        print("-" * 40)
+        final_output(item, possible_plaintext, mode="2") 
+        print("-" * 40)
+        print()
